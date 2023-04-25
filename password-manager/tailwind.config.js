ce0b598aaa41397e233ebd8de04540c876b5c885
Ritvik Shukla <ritvikshukla261@gmail.com>
2023-04-25 14:54:07 +0530

    add splash screen

---
+++ password-manager/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{ts,tsx,js,jsx}", "./splash-screen.html"],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
