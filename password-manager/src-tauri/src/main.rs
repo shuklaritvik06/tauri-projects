e603ddb2223a2b61da28fbf02c72ad75357327b8
Ritvik Shukla <ritvikshukla261@gmail.com>
2023-04-25 15:09:30 +0530

    initialize screens

--- password-manager/src-tauri/src/main.rs
+++ password-manager/src-tauri/src/main.rs
@@ -9,20 +9,8 @@ async fn close_splashscreen(window: tauri::Window) {
 }
 
 fn main() {
-  tauri::Builder::default()
+ tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![close_splashscreen])
-    .setup(|app| {
-      let splashscreen_window = app.get_window("splashscreen").unwrap();
-      let main_window = app.get_window("main").unwrap();
-      tauri::async_runtime::spawn(async move {
-        println!("Initializing...");
-        std::thread::sleep(std::time::Duration::from_secs(2));
-        println!("Done initializing.");
-        splashscreen_window.close().unwrap();
-        main_window.show().unwrap();
-      });
-      Ok(())
-    })
     .run(tauri::generate_context!())
     .expect("failed to run app");
 }
