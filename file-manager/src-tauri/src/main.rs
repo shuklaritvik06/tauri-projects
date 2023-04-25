8281b6ff92bfe2c66e2b7c488ff05890791ef0b6
Ritvik Shukla <ritvikshukla261@gmail.com>
2023-04-25 16:22:18 +0530

    initialize screens

--- file-manager/src-tauri/src/main.rs
+++ file-manager/src-tauri/src/main.rs
@@ -1,15 +1,16 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use tauri::Manager;
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn close_splashscreen(window: tauri::Window) {
+  if let Some(splashscreen) = window.get_window("splashscreen") {
+    splashscreen.close().unwrap();
+  }
+  window.get_window("main").unwrap().show().unwrap();
 }
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+ tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![close_splashscreen])
+    .run(tauri::generate_context!())
+    .expect("failed to run app");
 }
